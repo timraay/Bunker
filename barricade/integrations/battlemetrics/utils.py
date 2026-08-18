cf9a4b7faf8f47392e36b24604ded9946e79d30f
Tim Raaymakers <43066279+timraay@users.noreply.github.com>
2026-08-18 15:13:37 +0200

    Filter EOS ID by game

--- barricade/integrations/battlemetrics/utils.py
+++ barricade/integrations/battlemetrics/utils.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from barricade.enums import PlayerIDType
 
 
@@ -6,13 +8,24 @@ def find_player_id_in_attributes(attrs: dict) -> tuple[str | None, PlayerIDType]
     player_id_type = PlayerIDType.STEAM_64_ID
 
     # Find identifier of valid type
-    identifiers = attrs["identifiers"]
+    identifiers: list[dict[str, Any]] = attrs["identifiers"]
     for identifier_data in identifiers:
         try:
             # TODO: Add HLL & HLLV EOS IDs
             player_id_type = PlayerIDType(identifier_data["type"])
         except ValueError:
             continue
+
+        # If identifier is EOS ID, skip if not from HLL:V
+        is_manual = bool(identifier_data.get("manual", False))
+        metadata: dict[str, Any] = identifier_data.get("metadata", {})
+        if (
+            player_id_type == PlayerIDType.EOS_ID
+            and metadata.get("game") != "hllv"
+            and not is_manual
+        ):
+            continue
+
         player_id = identifier_data["identifier"]
         break
 
